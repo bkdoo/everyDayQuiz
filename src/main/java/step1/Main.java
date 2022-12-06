29eff2483a3a75a3c3f61256543bf41e8a4bf6eb
bkdoo <kyeongdoos@gmail.com>
2022-12-06 23:27:27 +0900

    2022 1206 백준 3003 문제 String을 받아와 int형으로 변환 후 간단한 사칙연산 후에 String으로 재 변환 후 리턴 Java Stream 기능 복습 하게 됨

--- src/main/java/step1/Main.java
+++ src/main/java/step1/Main.java
@@ -1,5 +1,7 @@
 package step1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,16 +9,41 @@ public class Main {
     public static void main(String[] args) {
 
 
-        baek2588();
+        baek3003();
 
 
     }
 
+    /**
+     * 2022 1206 백준 3003 문제
+     * String을 받아와 int형으로 변환 후
+     * 간단한 사칙연산 후에 String으로 재 변환 후 리턴
+     * Java Stream 기능 복습 하게 됨
+     */
+    private static void baek3003() {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+        String[] list = input.split(" ");
+
+        int[] array = Arrays.stream(list).mapToInt(Integer::parseInt).toArray();
+
+        int[] chess = {1,1,2,2,2,8};
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(chess[i]-array[i]+" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * 2022 1206 백준 2588 문제
+     * 가벼운 사칙연산 문제이나, 연산기호 간 우선순위를 체크하게 됨
+     */
     private static void baek2588() {
-        /*
-        *
-        * 2022 1206 백준 2588 문제
-        가벼운 사칙연산 문제이나, 연산기호 간 우선순위를 체크하게 됨*/
 
         Scanner sc = new Scanner(System.in);
 
